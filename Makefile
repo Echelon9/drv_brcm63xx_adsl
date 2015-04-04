92cfe88817028f99f82b09ead1cdc742b0e10d0c
Echelon9 <rhyskidd@gmail.com>
2015-04-04 18:42:04 +1100

    Initial project commit and placeholder implementation

---
+++ Makefile
@@ -0,0 +1,35 @@
+CSOURCES=$(wildcard *.c *.h)
+
+ifneq ($(KERNELRELEASE),)
+	obj-m := brcm63xx_adsl_drv.o
+else
+	KERNELDIR ?= /lib/modules/$(shell uname -r)/build
+	PWD       := $(shell pwd)
+
+all:
+	$(MAKE) -C $(KERNELDIR) M=$(PWD) modules
+
+clean:
+	$(MAKE) -C $(KERNELDIR) M=$(PWD) clean
+endif
+
+
+## astyle      : fix most C code indentation and formatting
+astyle: $(CSOURCES)
+	astyle --style=linux --max-code-length=80 $(CSOURCES)
+
+## doxygen     : generate documentation of the C code
+doxygen: doc/doxygen/html/index.html
+
+doc/doxygen/html/index.html: ${CSOURCES}
+	mkdir -p doc/doxygen
+	doxygen
+
+## cppcheck    : check for many code issues
+cppcheck: ${CSOURCES}
+	cppcheck ${CSOURCES} --enable=all --platform=unix64 \
+	--std=c99 --inline-suppr --quiet 
+
+## sloccount   : count lines of code
+sloccount: 
+	sloccount .
---
+++ brcm63xx_adsl_drv.c
@@ -0,0 +1,39 @@
+/*
+    brcm63xx_adsl_drv
+    Copyright (C) 2015  Rhys Kidd <rhyskidd@gmail.com>
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License along
+    with this program; if not, write to the Free Software Foundation, Inc.,
+    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+*/
+
+#include <linux/module.h>
+#include <linux/kernel.h>
+#include <linux/init.h>
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Rhys Kidd");
+
+static int __init adsl_init(void)
+{
+    printk(KERN_INFO "Hello adsl world.\n");
+    return 0;
+}
+
+static void __exit adsl_cleanup(void)
+{
+    printk(KERN_INFO "Cleaning up adsl module.\n");
+}
+
+module_init( adsl_init );
+module_exit( adsl_cleanup );
